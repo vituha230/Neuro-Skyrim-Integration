6511b8c1950a0524125d8e88380dd2ebffeb16a9
vituha230 <84087671+vituha230@users.noreply.github.com>
2026-05-24 20:26:07 +0500

    fixed raycasting for "small" alch lab and ench table

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -4727,6 +4727,24 @@ namespace MiscThings {
                             result = rotated_shift_vector;
                         }
 
+                        if (model.find("AlchemyWorkstation") != std::string::npos) //exclude markers. for some reason their model state is not 0 even though the model doesnt exist
+                        {
+                            RE::NiPoint3 base_shift_vector = { 10.0f, 60.0f, 75.0f };
+                            //RE::NiPoint3 base_shift_vector = { 10.0f, 55.0f, 75.0f };
+                            //RE::NiPoint3 base_shift_vector = { 0.0f, 0.0f, 1.0f };
+                            RE::NiPoint3 rotated_shift_vector = rotate_vector_by_angles(base_shift_vector, object_angles);
+                            result = rotated_shift_vector;
+                        }
+
+                        if (model.find("EnchantingWorkStation") != std::string::npos) //exclude markers. for some reason their model state is not 0 even though the model doesnt exist
+                        {
+                            RE::NiPoint3 base_shift_vector = { 0.0f, 40.0f, 57.5f };
+                            RE::NiPoint3 rotated_shift_vector = rotate_vector_by_angles(base_shift_vector, object_angles);
+                            result = rotated_shift_vector;
+                        }
+
+
+
                         if (model.find("EnchantingWorkbench") != std::string::npos) //exclude markers. for some reason their model state is not 0 even though the model doesnt exist
                         {
                             RE::NiPoint3 base_shift_vector = { 0.0f, 40.0f, 55.0f };
