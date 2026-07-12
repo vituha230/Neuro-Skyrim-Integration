f0422a707d4bce1ee4c05a03d5f00128d7f5b873
vituha230 <84087671+vituha230@users.noreply.github.com>
2026-07-12 18:07:03 +0500

    dont check if player has weapons in spell_mode and shout_mode for walking

--- src/WalkerProcessor.cpp
+++ src/WalkerProcessor.cpp
@@ -2531,7 +2531,7 @@ namespace WalkerProcessor {
                 //right_attack_cancel();
                 //left_attack_cancel();
 
-                bool brought_no_weapons_to_a_fight = MiscThings::has_spell_equipped(true) && MiscThings::has_spell_equipped(false) && !MiscThings::is_offensive_spell(true) && !MiscThings::is_offensive_spell(false);
+                bool brought_no_weapons_to_a_fight = !shout_mode && !spell_mode && MiscThings::has_spell_equipped(true) && MiscThings::has_spell_equipped(false) && !MiscThings::is_offensive_spell(true) && !MiscThings::is_offensive_spell(false);
 
                 if (last_walk_reminded_time > 40.0f || ((explore_mode || runaway_mode) && last_walk_reminded_time > 15.0f) || (interaction_after_walk == 3 && brought_no_weapons_to_a_fight && last_walk_reminded_time > 15.0f))
                 {
